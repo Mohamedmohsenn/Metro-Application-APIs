d6f5223ab6755e1b32703cf98a89c311f2e5491f
Ahmed Ibrahim <ahibrahim.ai7@gmail.com>
2021-07-23 14:52:25 +0200

    change user name,password,phone,email,date

--- METRO APPLICATION API’S/src/main/java/com/example/metroapp/controller/UserController.java
+++ METRO APPLICATION API’S/src/main/java/com/example/metroapp/controller/UserController.java
@@ -2,16 +2,18 @@ package com.example.metroapp.controller;
 
 import com.example.metroapp.interfaces.IUserService;
 import com.example.metroapp.model.Ticket;
+import com.example.metroapp.model.User;
+import com.example.metroapp.repository.UserRepo;
 import com.example.metroapp.security.jwt.JwtUtils;
 import com.example.metroapp.security.services.UserDetailsImpl;
 import com.example.metroapp.security.services.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -27,7 +29,6 @@ public class UserController {
     private UserDetailsServiceImpl userDetailsService;
 
 
-
     @GetMapping("/GetUserBalance")
     public ResponseEntity<?> getUserBalance(@RequestHeader String Authorization){
 
@@ -39,5 +40,69 @@ public class UserController {
         Map<String, Double> mp = new HashMap<>();
         mp.put("userBalance",balance);
         return new ResponseEntity<>(mp, HttpStatus.OK);
+    }
+    @PostMapping("/ChangeUserPassword")
+    public ResponseEntity<?> changeUserPassword(@RequestHeader String Authorization, @RequestBody String oldPassword,@RequestBody String newPassword){
+        Map<String, String> mp = new HashMap<>();
+        String Header[] = Authorization.split(" ");
+        String username = jwtUtils.getUserNameFromJwtToken(Header[1]);
+        if(userService.changeUserPassword(username,oldPassword,newPassword)){
+            mp.put("message","User Password updated successfully");
+            return new ResponseEntity<>(mp, HttpStatus.OK);
+        }
+        mp.put("message","Old password is wrong , try again");
+        return new ResponseEntity<>(mp, HttpStatus.BAD_REQUEST);
+    }
+
+    @PostMapping("/ChangeUserEmail")
+    public ResponseEntity<?> changeUserEmail(@RequestHeader String Authorization, @RequestBody String password,@RequestBody String newEmail){
+        Map<String, String> mp = new HashMap<>();
+        String Header[] = Authorization.split(" ");
+        String username = jwtUtils.getUserNameFromJwtToken(Header[1]);
+        if(userService.changeUserEmail(username,password,newEmail)){
+            mp.put("message","User Email updated successfully");
+            return new ResponseEntity<>(mp, HttpStatus.OK);
+        }
+        mp.put("message","User password is wrong , try again");
+        return new ResponseEntity<>(mp, HttpStatus.BAD_REQUEST);
+    }
+
+    @PostMapping("/ChangeUserPhoneNum")
+    public ResponseEntity<?> changeUserPhoneNum(@RequestHeader String Authorization, @RequestBody String password,@RequestBody String newPhoneNum){
+        Map<String, String> mp = new HashMap<>();
+        String Header[] = Authorization.split(" ");
+        String username = jwtUtils.getUserNameFromJwtToken(Header[1]);
+        if(userService.changeUserPhoneNum(username,password,newPhoneNum)){
+            mp.put("message","User Phone Number updated successfully");
+            return new ResponseEntity<>(mp, HttpStatus.OK);
+        }
+        mp.put("message","Old password is wrong , try again");
+        return new ResponseEntity<>(mp, HttpStatus.BAD_REQUEST);
+    }
+
+    @PostMapping("/ChangeUserName")
+    public ResponseEntity<?> changeUserName(@RequestHeader String Authorization, @RequestBody String newName){
+        Map<String, String> mp = new HashMap<>();
+        String Header[] = Authorization.split(" ");
+        String username = jwtUtils.getUserNameFromJwtToken(Header[1]);
+        if(userService.changeUserFullName(username,newName)){
+            mp.put("message","User Name updated successfully");
+            return new ResponseEntity<>(mp, HttpStatus.OK);
+        }
+        mp.put("message","Faild");
+        return new ResponseEntity<>(mp, HttpStatus.BAD_REQUEST);
+    }
+
+    @PostMapping("/ChangeUserBirthDate")
+    public ResponseEntity<?> changeUserBirthDate(@RequestHeader String Authorization,@RequestBody Date newDate){
+        Map<String, String> mp = new HashMap<>();
+        String Header[] = Authorization.split(" ");
+        String username = jwtUtils.getUserNameFromJwtToken(Header[1]);
+        if(userService.changeUserDate(username,newDate)){
+            mp.put("message","User Birth Date updated successfully");
+            return new ResponseEntity<>(mp, HttpStatus.OK);
+        }
+        mp.put("message","Faild");
+        return new ResponseEntity<>(mp, HttpStatus.BAD_REQUEST);
     }
 }
--- METRO APPLICATION API’S/src/main/java/com/example/metroapp/service/UserService.java
+++ METRO APPLICATION API’S/src/main/java/com/example/metroapp/service/UserService.java
@@ -4,14 +4,130 @@ import com.example.metroapp.interfaces.IUserService;
 import com.example.metroapp.model.User;
 import com.example.metroapp.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 public class UserService implements IUserService {
-@Autowired
+    @Autowired
     UserRepo userRepo;
+    @Autowired
+    PasswordEncoder encoder;
+
+    @Override
     public Double getUserBalance(Integer userID){
         User user = userRepo.getById(userID);
         return user.getBalance();
+    }
+    @Override
+    public Boolean changeUserPassword(String username,String oldPassword,String newPassword){
+
+        User user=userRepo.findByUsername(username)
+                .orElse(null);
+        if (user == null)
+        {
+            return false;
+        }
+        String userPassword = user.getPassword();
+        oldPassword=encoder.encode(oldPassword);
+        newPassword=encoder.encode(newPassword);
+        if(oldPassword.equals(userPassword)){
+            try {
+                user.setPassword(newPassword);
+                userRepo.save(user);
+                return true;
+            }
+            catch (Exception e){
+                return false;
+            }
+        }
+        return false;
+    }
+    @Override
+    public Boolean changeUserEmail(String username,String password,String newEmail){
+
+        User user= userRepo.findByUsername(username)
+                .orElse(null);
+        if (user == null)
+        {
+            return false;
+        }
+        String userPassword = user.getPassword();
+        password=encoder.encode(password);
+        if(password.equals(userPassword)){
+            try {
+                user.setEmail(newEmail);
+                userRepo.save(user);
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+        return false;
+    }
+    @Override
+    public Boolean changeUserPhoneNum(String username,String password,String newPhoneNum){
+
+        User user= userRepo.findByUsername(username)
+                .orElse(null);
+        if (user == null)
+        {
+            return false;
+        }
+        String userPassword = user.getPassword();
+        password=encoder.encode(password);
+        if(password.equals(userPassword)){
+            try {
+                user.setPhone_number(newPhoneNum);
+                userRepo.save(user);
+                return true;
+            }
+            catch (Exception e){
+                return false;
+            }
+        }
+        return false;
+    }
+    @Override
+    public Boolean changeUserFullName(String username,String newName){
+        User user= userRepo.findByUsername(username)
+                .orElse(null);
+        if (user == null)
+        {
+            return false;
+        }
+        try {
+            user.setFullname(newName);
+            userRepo.save(user);
+            return true;
+        }
+        catch (Exception e)
+        {
+            return false;
+        }
+    }
+    @Override
+    public Boolean changeUserDate(String username, Date newDate){
+        User user= userRepo.findByUsername(username)
+                .orElse(null);
+        if (user == null)
+        {
+            return false;
+        }
+        try {
+            user.setDate_of_birth(newDate);
+            userRepo.save(user);
+            return true;
+        }
+        catch (Exception e)
+        {
+            return false;
+        }
     }
 }
