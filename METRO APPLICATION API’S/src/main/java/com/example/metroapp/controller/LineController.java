8ca79c774053e30febb2428865522cbff33c3c13
Mohamed mohsen <36299904+Mohamedmohsenn@users.noreply.github.com>
2021-07-24 04:10:24 +0200

    Edit get all station API and get Ticket price

--- METRO APPLICATION API’S/src/main/java/com/example/metroapp/controller/LineController.java
+++ METRO APPLICATION API’S/src/main/java/com/example/metroapp/controller/LineController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,7 +25,7 @@ public class LineController {
 
     @CrossOrigin
     @PreAuthorize("hasAnyRole('user')")
-    @RequestMapping("/GetAllLines")
+    @GetMapping("/GetAllLines")
     public ResponseEntity<?> getAllLines()
     {
         Map<String, List<Line>> map = new HashMap<>();
